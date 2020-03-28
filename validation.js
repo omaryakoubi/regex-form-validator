3a4bd7a14717fc5aff10fb1acc03c5b1af5ab066
Firas Mrad <firasmr997@gmail.com>
2020-03-28 18:22:32 +0100

    Finish

--- validation.js
+++ validation.js
@@ -1,1 +1,65 @@
-// validation script here
+document.getElementById('username').addEventListener('sign', validateUsername);
+document.getElementById('email').addEventListener('sign', validateEmail);
+document.getElementById('password1').addEventListener('sign', validatePassword);
+document.getElementById('phoneNumber').addEventListener('sign', validatePhone);
+document.getElementById('cardNumber').addEventListener('sign', validateCard);
+
+function validateUsername() {
+    const user = document.getElementById('username');
+    const val = /^(([a-z])(?=.*[0-9])(?=.{4,12}))/;
+    var text;
+    if(!val.test(user.value)) {
+        text = 'Must be aphanumeric and contains 5-12 characters. Ex: walid123'
+    } else {
+        text = ''
+    }
+     document.getElementById("errorUsername").innerHTML = text;
+}
+
+function validateEmail() {
+    const email = document.getElementById('email');
+    const val = /^([a-zA-Z0-9_\.\-]+)@([a-zA-Z0-9_\.\-]+)\.([a-zA-Z]{2,5})$/;
+    var text;
+    if(!val.test(email.value)) {
+        text = 'Email must be a valid address, e.g. me@mydomain.com';
+    } else {
+      text = " "
+    }
+    document.getElementById("errorEmail").innerHTML = text;
+}
+
+function validatePassword() {
+    const password = document.getElementById('password1');
+    const val = /^(([a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{4,12}))/;
+    var text;
+    if(!val.test(password.value)) {
+        text = 'Must be alphanumeric contain 5-12 characters,must have uppercase \n and lowercase ex: WAlid123';
+    } else {
+        text = ' '
+    }
+    document.getElementById("errorPassword").innerHTML = text;
+}
+
+function validatePhone() {
+    const phone = document.getElementById('phoneNumber');
+    const val = /^(00216)\d{8}$/;
+    var text;
+    if(!val.test(phone.value)) {
+        text = 'Must be a valid Tunisia number (13 digit) ex:(0021620200200)'
+    } else {
+        text = ' '
+    }
+    document.getElementById("errorPhonenumber").innerHTML = text;
+}
+
+function validateCard() {
+    const card = document.getElementById('cardNumber');
+    const val = /^\(?\d{2}\)?[.-]?\d{2}[.-]?\d{2}$/;
+    var text;
+    if(!val.test(card.value)) {
+        text = 'Must be numeric(6-digit) between every two digit (-) ex: 12-35-89'
+    } else {
+        text = ' '
+    }
+    document.getElementById("errorCardnumber").innerHTML = text;
+}
