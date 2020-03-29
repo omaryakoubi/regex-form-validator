fd76eb0f9c14cff23378ef7b61fd7045b38be71a
omaryakoubi <yakoubiomar2001@gmail.com>
2020-03-29 16:52:33 +0100

    /Finished

--- validation.js
+++ validation.js
@@ -1,65 +1,78 @@
-document.getElementById('username').addEventListener('blur', validateUsername);
-document.getElementById('email').addEventListener('blur', validateEmail);
-document.getElementById('password1').addEventListener('blur', validatePassword);
-document.getElementById('phoneNumber').addEventListener('blur', validatePhone);
-document.getElementById('cardNumber').addEventListener('blur', validateCard);
+document.getElementById("username").addEventListener('blur',checkUserName)
+document.getElementById("email").addEventListener('blur',checkEmail)
+document.getElementById("password").addEventListener('blur',checkPassword)
+document.getElementById("phoneNumber").addEventListener('blur',checkPhone)
+document.getElementById("cardNumber").addEventListener('blur',checkCardNumber)
 
-function validateUsername() {
-    const user = document.getElementById('username');
-    const val = /^(([a-z])(?=.*[0-9])(?=.{4,12}))/;
-    var text;
-    if(!val.test(user.value)) {
-        text = 'Must be aphanumeric and contains 5-12 characters. Ex: walid123'
-    } else {
-        text = ''
-    }
-     document.getElementById("errorUsername").innerHTML = text;
-}
 
-function validateEmail() {
-    const email = document.getElementById('email');
-    const val = /^([a-zA-Z0-9_\.\-]+)@([a-zA-Z0-9_\.\-]+)\.([a-zA-Z]{2,5})$/;
-    var text;
-    if(!val.test(email.value)) {
-        text = 'Email must be a valid address, e.g. me@mydomain.com';
-    } else {
-      text = " "
+function checkUserName(){
+    var paragraph = document.getElementById("errorUsername")
+    var user = document.getElementById('username');
+    var nameRegex = /^[a-z0-9]{5,12}$/i;
+    
+    if(nameRegex.test(user.value)){
+        user.style.border = " solid green";
+        paragraph.style.visibility = "hidden";
+    }else{
+        user.style.border = "solid red";
+        paragraph.style.visibility = "visible";
     }
-    document.getElementById("errorEmail").innerHTML = text;
-}
+};
 
-function validatePassword() {
-    const password = document.getElementById('password1');
-    const val = /^(([a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{4,12}))/;
-    var text;
-    if(!val.test(password.value)) {
-        text = 'Must be alphanumeric contain 5-12 characters,must have uppercase \n and lowercase ex: WAlid123';
-    } else {
-        text = ' '
+function checkEmail(){
+    var paragraph = document.getElementById("errorEmail")
+    var email = document.getElementById('email');
+    var emailRegex = /^[a-z\d_.]{2,}@[a-z]{3,}.[a-z]{2,3}$/;
+    
+    if(emailRegex.test(email.value)){
+        email.style.border = " solid green"
+        paragraph.Style.visibility = "hidden" 
+    }else{
+        email.style.border = "solid red"
+        paragraph.Style.visibility = "visible" 
     }
-    document.getElementById("errorPassword").innerHTML = text;
-}
+};
 
-function validatePhone() {
-    const phone = document.getElementById('phoneNumber');
-    const val = /^(00216)\d{8}$/;
-    var text;
-    if(!val.test(phone.value)) {
-        text = 'Must be a valid Tunisia number (13 digit) ex:(0021620200200)'
-    } else {
-        text = ' '
-    }
-    document.getElementById("errorPhonenumber").innerHTML = text;
-}
 
-function validateCard() {
-    const card = document.getElementById('cardNumber');
-    const val = /^\(?\d{2}\)?[.-]?\d{2}[.-]?\d{2}$/;
-    var text;
-    if(!val.test(card.value)) {
-        text = 'Must be numeric(6-digit) between every two digit (-) ex: 12-35-89'
-    } else {
-        text = ' '
+function checkPassword(){
+    var paragraph = document.getElementById("errorPassword")
+    var password = document.getElementById('password');
+    var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    
+    if(passwordRegex.test(password.value)){
+        password.style.border = " solid green"
+        paragraph.Style.visibility = "hidden" 
+    } else{
+        password.style.border = "solid red"
+        paragraph.Style.visibility = "visible" 
     }
-    document.getElementById("errorCardnumber").innerHTML = text;
-}
+} ;
+
+function checkPhone(){
+    var paragraph = document.getElementById("errorPhonenumber")
+    var phone = document.getElementById("phoneNumber");
+    var phoneRegex = /^00216[0-9]{8}$/;
+    
+    if(phoneRegex.test(phone.value)){
+        phone.style.border = " solid green"
+        paragraph.Style.visibility = "hidden" 
+    } else {
+        phone.style.border = "solid red"
+        paragraph.Style.visibility = "visible" 
+    }
+};
+
+function checkCardNumber(){
+    var paragraph = document.getElementById("errorCardnumber")
+    var cardNumber = document.getElementById('cardNumber');
+    var cardNumberRegex = /^\d{2}\-\d{2}\-\d{2}$/;
+    
+    if(cardNumberRegex.test(cardNumber.value)){
+        cardNumber.style.border = "solid green"
+        paragraph.style.visibility = "hidden" 
+    } else{ 
+        cardNumber.style.border = "solid red"
+        paragraph.style.visibility = "visible" 
+    }
+};
+
